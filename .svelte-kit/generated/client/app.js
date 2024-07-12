ef6eaae9073effe79c2091eca4e4fd342f27b6a4
danielchavesdev <daniel.cassiano@live.com>
2024-07-12 20:22:52 -0300

    chore: cleanup

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,27 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [4],
+		"/kitchen-sink": [6],
+		"/(app)/permutas": [~5,[2],[3]]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
