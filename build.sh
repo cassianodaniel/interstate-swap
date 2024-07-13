ed788fc1f2ee23a03006e1cd486460581f1eb1c1
danielchavesdev <daniel.cassiano@live.com>
2024-07-13 13:20:42 -0300

    chore: build

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ "$VERCEL_ENV" == "production" ]
-then
-  npx @edgedb/generate@0.4.1 edgeql-js --output-dir ./edgedb --target esm --tls-security insecure -d production
-else
-	npx @edgedb/generate@0.4.1 edgeql-js --output-dir ./edgedb --target esm --tls-security insecure -d staging
-fi
+# if [ "$VERCEL_ENV" == "production" ]
+# then
+#   npx @edgedb/generate@0.4.1 edgeql-js --output-dir ./edgedb --target esm --tls-security insecure -d production
+# else
+	# npx @edgedb/generate@0.4.1 edgeql-js --output-dir ./edgedb --target esm --tls-security insecure -d staging
+# fi
+
+npx @edgedb/generate@0.4.1 edgeql-js --output-dir ./edgedb --target esm --tls-security insecure -d main
